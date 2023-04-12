82d0bae1b704f563932eca47d7392f694303e93e
rishavjha546 <80584202+rishavjha546@users.noreply.github.com>
2023-04-12 11:01:52 +0530

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -3,10 +3,10 @@ from pydub import AudioSegment
 
 r = sr.Recognizer()
 
-sound = AudioSegment.from_mp3("Sharmishtha.mp3")
-sound.export("Sharmishtha.wav", format="wav")
+sound = AudioSegment.from_mp3("Sharmishtha (1).mp3")
+sound.export("Sharmishtha (1).wav", format="wav")
 
-with sr.AudioFile('Sharmishtha.wav') as source:
+with sr.AudioFile('Sharmishtha (1).wav') as source:
     audio_text = r.record(source)
     text = r.recognize_google(audio_text)
     print(text)
